ea7c97afb658aaeec265e8f37a5ca6ff252a866a
Haroon <you@example.com>
2024-08-27 11:56:16 +0300

    Add exercises 8 and 9

--- exercises.py
+++ exercises.py
@@ -139,3 +139,54 @@ def calculate_tip(amount,tipPercentage):
     return amount * tipPercentage / 100
 
 print('Exercise 7:', calculate_tip(50, 20))
+
+
+# Exercise 8: Calculate Product of Numbers
+#
+# Write a function named `product` that takes an arbitrary number of numbers, multiplies them, and returns the product.
+# Review your notes on *args for handling an arbitrary number of arguments.
+#
+# Examples:
+# product(-1, 4) should return -4.
+# product(2, 5, 5) should return 50.
+#
+# Define the function and call it with different sets of numbers to test.
+
+def product(*product):
+    result = 1
+    for i in product:
+        result *= i
+    return result
+
+print('Exercise 8:', product(2, 5, 5))
+
+
+# Exercise 9: Basic Calculator
+#
+# Create a function named `basicCalculator` that takes three arguments: 
+# two numbers and a string representing an operation ('add', 'subtract', 'multiply', 'divide'). 
+# Perform the provided operation on the two numbers. In operations where the order of numbers is important, 
+# treat the first parameter as the first operand and the second parameter as the second operand.
+#
+# Examples:
+# basicCalculator(10, 5, 'subtract') should return 5.
+# basicCalculator(10, 5, 'add') should return 15.
+# basicCalculator(10, 5, 'multiply') should return 50.
+# basicCalculator(10, 5, 'divide') should return 2.
+#
+# Define the function and then call it below.
+
+def basicCalculator(num1,num2,operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        return num1 / num2
+
+print('Exercise 9 Result:', basicCalculator(10, 5, "subtract"))
+print('Exercise 9 Result:', basicCalculator(10, 5, "add"))
+print('Exercise 9 Result:', basicCalculator(10, 5, "multiply"))
+print('Exercise 9 Result:', basicCalculator(10, 5, "divide"))
